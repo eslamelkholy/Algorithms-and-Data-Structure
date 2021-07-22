ae59ef18ae8d818d5f81bc8b838d3505071e6e15
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-07-22 17:27:27 +0200

    Added Hash Table Data Structure With Simple Example

---
+++ 5-HashTable/1-HashTable.js
@@ -0,0 +1,11 @@
+/**
+ * Hash Table is One Of The Common Used Data Structure
+ * where you can Access Value By O(1) Because it Consist of Key Value Pairs
+ * Also Known As
+ * HashMap, Map, Dictionaries, Associated Array and any other thing using Key Value to Access Data
+ */
+
+const book = new Map();
+book.set('apple', '4$');
+book.set('orange', '10$');
+console.log(book.get('apple'));
